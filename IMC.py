43306ec3d26753e363a7eb2a58d953b657ff48c1
Elias-Almenda <eliasalmenda64@gmail.com>
2026-03-18 21:00:48 -0300

    Primeiro commit do curso

---
+++ IMC.py
@@ -0,0 +1,6 @@
+nome =input ('Digite seu nome: ')
+peso = float(input (('Digite seu peso: ')))
+altura = float(input ("digite sua altura: "))
+IMC = peso / (altura * altura)
+
+print(f"O IMC do usuário {nome} é {IMC}")
---
+++ interpolacao_string.py
@@ -0,0 +1,7 @@
+nome = input("Qual seu nome: ")
+preco = float(input("Qual o valor: "))
+
+variavel = "%s, o preco é R$%.2f" % (nome, preco)
+
+print(variavel)
+print('O hexadecimal de %d é %08X' % (1500, 1500))
