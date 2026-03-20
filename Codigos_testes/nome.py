f7b94e0f4d18fed0e710fd6f8e1394bc5dd931cc
Elias-Almenda <eliasalmenda64@gmail.com>
2026-03-20 17:34:17 -0300

    ajustando

--- Codigos_testes/nome.py
+++ Codigos_testes/nome.py
@@ -2,17 +2,22 @@
 
 nome = input('Digite o seu nome: ')
 idade = input('Digite a sua idade: ')
-letra = nome[::]
 
+erro = False
 
 if not nome or not idade:
     print('Desculpe, você deixou campos vazios.')
-elif '' in nome:
+    erro = True
+    
+elif ' ' in nome:
     print('Seu nome contem espaços')
-    
-    
-    
+    erro = True
 
-print(f"Seu nome é {nome} ele invertido é {nome[::-1]} , seu nome tem essa quantidade de letras= {len(nome)}")
-print(f'A primeira letra do seu nome é {letra[0:1]}')
-print(f'A ultima letra do seu nome é {nome[-1]}')
+
+print(f"""
+Nome: {nome}
+Invertido: {nome[::-1]}
+Quantidade de letras: {len(nome)}
+Primeira letra: {nome[0]}
+Última letra: {nome[-1]} 
+""")
