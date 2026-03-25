184b56b9d50e916c30db631c1551bef1b0c68065
Elias-Almenda <eliasalmenda64@gmail.com>
2026-03-25 20:58:30 -0300

    testes

--- Codigos_testes/while.py
+++ Codigos_testes/while.py
@@ -10,11 +10,42 @@ condicao = True
     
     
     
-contador = int(input("Onde você gostaria que seu contador começase:  "))
-tamanho_contador = int(input("Ate quanto você gostaria de contar: "))
-while contador <= tamanho_contador -1 :
-    contador = contador + 1
-    print(contador)
+# contador = 0
+
+# while contador <= 100:
+#     contador += 1
+    
+#     if contador == 6:
+#         print("Não quero o 6")
+#         continue
+    
+#     if contador >= 10 and contador <= 27:
+#         print("Quero não")
+#         continue
+    
+#     print(contador)
+    
+    
+#     if contador == 40:
+#        break
 
 
-print("finalizou")
+# print("finalizou")
+
+qtd_linhas = 5
+qtd_colunas = 5
+
+linha = 1
+while linha <= qtd_linhas:
+    colunas = 1
+    
+    
+    while colunas <= qtd_colunas:
+        print(f"{linha=},{colunas=}")
+        colunas += 1
+    linha +=1
+    
+
+print("Acabou as linhas e colunas")
+
+
